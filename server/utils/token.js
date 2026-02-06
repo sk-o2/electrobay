9df6d2f25e1cb2c485be905c13f5f1b436304cfa
Sk Thakur <sumit.here19@gmail.com>
2026-02-06 09:30:11 +0530

    Fix frontend build with Vite

--- server/utils/token.js
+++ server/utils/token.js
@@ -16,20 +16,36 @@
 //   });
 // };
 
-export const setAuthCookies = (res, accessToken, refreshToken) => {
-  const isProd = process.env.NODE_ENV === "production";
+// export const setAuthCookies = (res, accessToken, refreshToken) => {
+//   const isProd = process.env.NODE_ENV === "production";
 
+//   res.cookie("accessToken", accessToken, {
+//     httpOnly: true,
+//     secure: isProd,              //http ke liye false, https ke liye true
+//     sameSite: isProd ? "none" : "lax",   //production me none, development me lax
+//     maxAge: 15 * 60 * 1000,
+//   });
+
+//   res.cookie("refreshToken", refreshToken, {
+//     httpOnly: true,
+//     secure: isProd,
+//     sameSite: isProd ? "none" : "lax",
+//     maxAge: 30 * 24 * 60 * 60 * 1000,
+//   });
+// };
+
+export const setAuthCookies = (res, accessToken, refreshToken) => {
   res.cookie("accessToken", accessToken, {
     httpOnly: true,
-    secure: isProd,              //http ke liye false, https ke liye true
-    sameSite: isProd ? "none" : "lax",   //production me none, development me lax
+    secure: true,
+    sameSite: "none",
     maxAge: 15 * 60 * 1000,
   });
 
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
-    secure: isProd,
-    sameSite: isProd ? "none" : "lax",
+    secure: true,
+    sameSite: "none",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 };
